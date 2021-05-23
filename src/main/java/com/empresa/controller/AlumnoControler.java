e108bb25263bdb16e9bddf92b8cb953c507c4036
betsi <betsypm>
2021-05-23 16:34:01 -0500

    eliminar

--- src/main/java/com/empresa/controller/AlumnoControler.java
+++ src/main/java/com/empresa/controller/AlumnoControler.java
@@ -1,11 +1,15 @@
 package com.empresa.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,4 +45,35 @@ public class AlumnoControler {
 			return ResponseEntity.badRequest().build();
 		} 
 	}
+
+	@PutMapping
+	public ResponseEntity<Alumno> actualiza(@RequestBody Alumno obj){
+		log.info(">>>> actualiza " + obj.getIdAlumno());
+		Optional<Alumno> optAlumno = service.obtienePorId(obj.getIdAlumno());
+		if (optAlumno.isPresent()) {
+			Alumno objSalida = service.insertaActualizaAlumno(obj);
+			if (objSalida != null) {
+				return ResponseEntity.ok(objSalida);
+			}else {
+				return ResponseEntity.badRequest().build();
+			}	
+		}else {
+			log.info(">>>> actualiza no existe el id : " + obj.getIdAlumno());
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Alumno> elimina(@PathVariable("id") int idAlumno){
+		log.info(">>>> elimina " + idAlumno);
+		Optional<Alumno> optAlumno = service.obtienePorId(idAlumno);
+		if (optAlumno.isPresent()) {
+			service.eliminaAlumno(idAlumno);
+			return ResponseEntity.ok(optAlumno.get());
+		}else {
+			log.info(">>>> elimina no existe el id : " + idAlumno);
+			return ResponseEntity.badRequest().build();
+		}
+	}
+
 }
